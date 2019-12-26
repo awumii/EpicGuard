9c5d2b641b8f9943aa41ccd59fdeeabb4f6f83df
PolskiStevek <polskistevek@op.pl>
2019-12-26 10:02:31 +0100

    Fix Layer 7 DDoS attacks.

--- src/main/java/io/github/polskistevek/epicguard/bukkit/listener/ServerListPingListener.java
+++ src/main/java/io/github/polskistevek/epicguard/bukkit/listener/ServerListPingListener.java
@@ -1,5 +1,6 @@
 package io.github.polskistevek.epicguard.bukkit.listener;
 
+import io.github.polskistevek.epicguard.bukkit.GuardBukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
@@ -11,8 +12,12 @@ public class ServerListPingListener implements Listener {
     @EventHandler
     public void onPing(ServerListPingEvent e) {
         try {
-            AttackManager.checkAttackStatus(AttackManager.AttackType.PING);
             AttackManager.handleAttack(AttackManager.AttackType.PING);
+            if (AttackManager.pingPerSecond > GuardBukkit.PING_SPEED) {
+                e.setMotd("");
+                e.setMaxPlayers(0);
+                e.setServerIcon(null);
+            }
         } catch (Exception ex) {
             Logger.throwException(ex);
         }
