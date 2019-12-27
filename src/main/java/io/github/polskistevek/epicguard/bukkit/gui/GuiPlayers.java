b1fb809e69e22e8b0e6cb483f745f3783be0f8aa
PolskiStevek <polskistevek@op.pl>
2019-12-27 20:45:46 +0100

    Catch exception in gui, redesign exception throwing.

--- src/main/java/io/github/polskistevek/epicguard/bukkit/gui/GuiPlayers.java
+++ src/main/java/io/github/polskistevek/epicguard/bukkit/gui/GuiPlayers.java
@@ -23,44 +23,44 @@ public class GuiPlayers {
     public static Inventory inv;
 
     public static void show(Player p) {
-        int i = 0;
-        for (Player player : Bukkit.getOnlinePlayers()) {
-            final ItemStack itemStack;
-            final List<String> lore = new ArrayList<>();
-            final User user = UserManager.getUser(player);
-            lore.add("");
-            lore.add(ChatUtil.fix("&7UUID: &3" + player.getUniqueId()));
-            lore.add(ChatUtil.fix("&7First Join: &6" + new Date(player.getFirstPlayed())));
-            try {
+        try {
+            int i = 0;
+            for (Player player : Bukkit.getOnlinePlayers()) {
+                final ItemStack itemStack;
+                final List<String> lore = new ArrayList<>();
+                final User user = UserManager.getUser(player);
+                lore.add("");
+                lore.add(ChatUtil.fix("&7UUID: &3" + player.getUniqueId()));
+                lore.add(ChatUtil.fix("&7First Join: &6" + new Date(player.getFirstPlayed())));
                 lore.add(ChatUtil.fix("&7Country: &6" + GeoAPI.getDatabase().country(player.getAddress().getAddress()).getCountry().getIsoCode()));
-            } catch (IOException | GeoIp2Exception e) {
-                Logger.throwException(e);
-            }
-            lore.add(ChatUtil.fix("&7OP: " + (player.isOp() ? "&aYes" : "&cNo")));
-            lore.add(ChatUtil.fix("&7Client Brand: &c" + user.getBrand()));
-            if (GuardBukkit.IP_HISTORY_ENABLE) {
-                if (user.getAdresses() != null) {
-                    lore.add("");
-                    lore.add(ChatUtil.fix("&7IP History:"));
-                    for (String adress : user.getAdresses()) {
-                        if (player.getAddress().getAddress().getHostAddress().equals(adress)) {
-                            lore.add(ChatUtil.fix("&8> &c" + adress + " &8(&aCurrent&8)"));
-                        } else {
-                            lore.add(ChatUtil.fix("&8> &c" + adress));
+                lore.add(ChatUtil.fix("&7OP: " + (player.isOp() ? "&aYes" : "&cNo")));
+                lore.add(ChatUtil.fix("&7Client Brand: &c" + user.getBrand()));
+                if (GuardBukkit.IP_HISTORY_ENABLE) {
+                    if (user.getAdresses() != null) {
+                        lore.add("");
+                        lore.add(ChatUtil.fix("&7IP History:"));
+                        for (String adress : user.getAdresses()) {
+                            if (player.getAddress().getAddress().getHostAddress().equals(adress)) {
+                                lore.add(ChatUtil.fix("&8> &c" + adress + " &8(&aCurrent&8)"));
+                            } else {
+                                lore.add(ChatUtil.fix("&8> &c" + adress));
+                            }
                         }
                     }
                 }
+                if (player.isOp()) {
+                    itemStack = new ItemBuilder(Material.DIAMOND_HELMET).setTitle("&c" + player.getName()).addLores(lore).build();
+                } else {
+                    itemStack = new ItemBuilder(Material.IRON_HELMET).setTitle("&a" + player.getName()).addLores(lore).build();
+                }
+                ItemStack back = new ItemBuilder(Material.ARROW).setTitle("&cBack to main menu").addLore("").addLore("&7Click to go back.").build();
+                inv.setItem(i, itemStack);
+                inv.setItem(35, back);
+                i++;
             }
-            if (player.isOp()) {
-                itemStack = new ItemBuilder(Material.DIAMOND_HELMET).setTitle("&c" + player.getName()).addLores(lore).build();
-            } else {
-                itemStack = new ItemBuilder(Material.IRON_HELMET).setTitle("&a" + player.getName()).addLores(lore).build();
-            }
-            ItemStack back = new ItemBuilder(Material.ARROW).setTitle("&cBack to main menu").addLore("").addLore("&7Click to go back.").build();
-            inv.setItem(i, itemStack);
-            inv.setItem(35, back);
-            i++;
+            p.openInventory(inv);
+        } catch (Exception e) {
+            Logger.throwException(e);
         }
-        p.openInventory(inv);
     }
 }
