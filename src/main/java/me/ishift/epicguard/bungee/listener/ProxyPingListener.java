843982f7d6bfaf19dad0d1baf68bd27e4cf66c4c
PolskiStevek <polskistevek@op.pl>
2020-01-01 09:52:27 +0100

    Layer-7 attack block.

--- src/main/java/me/ishift/epicguard/bungee/listener/ProxyPingListener.java
+++ src/main/java/me/ishift/epicguard/bungee/listener/ProxyPingListener.java
@@ -4,6 +4,8 @@ import me.ishift.epicguard.bungee.util.BungeeAttack;
 import me.ishift.epicguard.universal.AttackType;
 import me.ishift.epicguard.universal.Config;
 import me.ishift.epicguard.universal.util.Logger;
+import net.md_5.bungee.api.ServerPing;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -16,6 +18,9 @@ public class ProxyPingListener implements Listener {
                 BungeeAttack.handle(AttackType.PING);
                 if (BungeeAttack.getPingPerSecond() > Config.PING_SPEED) {
                     BungeeAttack.setAttack(true);
+                    final ServerPing response = event.getResponse();
+                    response.setDescriptionComponent(new TextComponent(""));
+                    event.setResponse(response);
                 }
             }
         } catch (Exception ex) {
