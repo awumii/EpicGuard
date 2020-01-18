07e5b322133c41d333375b9e180c674a5a7f840c
PolskiStevek <polskistevek@op.pl>
2020-01-18 11:46:37 +0100

    Simplified ConnectionCloser

--- src/main/java/me/ishift/epicguard/bungee/util/ConnectionCloser.java
+++ src/main/java/me/ishift/epicguard/bungee/util/ConnectionCloser.java
@@ -2,7 +2,7 @@ package me.ishift.epicguard.bungee.util;
 
 import me.ishift.epicguard.bungee.GuardBungee;
 import me.ishift.epicguard.universal.util.ChatUtil;
-import me.ishift.epicguard.universal.util.KickReason;
+import me.ishift.epicguard.universal.KickReason;
 import me.ishift.epicguard.universal.util.Logger;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.PendingConnection;
