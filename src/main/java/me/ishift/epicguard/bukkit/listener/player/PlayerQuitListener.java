8069e2c435ea0e19deb7206edfdea63fa6c12a27
PolskiStevek <polskistevek@op.pl>
2020-02-02 14:07:10 +0100

    Improved AntiBypass (deleted useless code).
    Signed-off-by: PolskiStevek <polskistevek@op.pl>
    
    Former-commit-id: 3e363d7b89dde532356274a728257f44dee9ed5f

--- src/main/java/me/ishift/epicguard/bukkit/listener/player/PlayerQuitListener.java
+++ src/main/java/me/ishift/epicguard/bukkit/listener/player/PlayerQuitListener.java
@@ -1,5 +1,6 @@
 package me.ishift.epicguard.bukkit.listener.player;
 
+import me.ishift.epicguard.bukkit.manager.BlacklistManager;
 import me.ishift.epicguard.bukkit.manager.UserManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,15 +12,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PlayerQuitListener implements Listener {
-    public static List<String> hiddenNames = new ArrayList<>();
-
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onQuit(PlayerQuitEvent e) {
-        Player p = e.getPlayer();
-        UserManager.removeUser(p);
-        if (hiddenNames.contains(p.getName())) {
-            e.setQuitMessage("");
-            hiddenNames.remove(p.getName());
+    public void onQuit(PlayerQuitEvent event) {
+        final Player player = event.getPlayer();
+        final String adress = player.getAddress().getAddress().getHostAddress();
+        UserManager.removeUser(player);
+        if (BlacklistManager.isBlacklisted(adress)) {
+            event.setQuitMessage("");
         }
     }
 }
