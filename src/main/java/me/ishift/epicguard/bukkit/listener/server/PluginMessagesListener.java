0ea6c180b53cce3c6ada48c28f998604f7e98b25
PolskiStevek <polskistevek@op.pl>
2020-02-02 14:26:16 +0100

    Remove useless catch, and log info.
    Signed-off-by: PolskiStevek <polskistevek@op.pl>
    
    Former-commit-id: 5f33bd19ed25ab3d95fd7990b65ee433a46cacb1

--- src/main/java/me/ishift/epicguard/bukkit/listener/server/PluginMessagesListener.java
+++ src/main/java/me/ishift/epicguard/bukkit/listener/server/PluginMessagesListener.java
@@ -2,32 +2,26 @@ package me.ishift.epicguard.bukkit.listener.server;
 
 import me.ishift.epicguard.bukkit.manager.UserManager;
 import me.ishift.epicguard.bukkit.manager.User;
-import me.ishift.epicguard.universal.util.Logger;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
+import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 
 public class PluginMessagesListener implements PluginMessageListener {
     public static void addChannel(Player p, String channel) {
         try {
             p.getClass().getMethod("addChannel", String.class).invoke(p, channel);
-        } catch (Exception e) {
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
-            Logger.info("DISABLE 'channel-verification' in brand.yml fil! Exception in class: " + e.getClass().getName());
         }
     }
 
     @SuppressWarnings("NullableProblems")
     @Override
     public void onPluginMessageReceived(String channel, Player player, byte[] bytes) {
-        try {
-            String brand = new String(bytes, StandardCharsets.UTF_8).substring(1);
-            User u = UserManager.getUser(player);
-            u.setBrand(brand);
-        } catch (Exception e) {
-            e.printStackTrace();
-            Logger.info("DISABLE 'channel-verification' in brand.yml fil! Exception in class: " + e.getClass().getName());
-        }
+        String brand = new String(bytes, StandardCharsets.UTF_8).substring(1);
+        User u = UserManager.getUser(player);
+        u.setBrand(brand);
     }
 }
