027186e2d6c287366481bc6a4c9769fbf280d152
PolskiStevek <polskistevek@op.pl>
2020-02-02 14:44:39 +0100

    Deleted useless code from cloud.
    Signed-off-by: PolskiStevek <polskistevek@op.pl>
    
    Former-commit-id: 1d4c0b8f92ac1c2259d6f855a11e318304b4dc2d

--- src/main/java/me/ishift/epicguard/bukkit/task/CloudTask.java
+++ src/main/java/me/ishift/epicguard/bukkit/task/CloudTask.java
@@ -3,18 +3,15 @@ package me.ishift.epicguard.bukkit.task;
 import me.ishift.epicguard.bukkit.manager.BlacklistManager;
 import me.ishift.epicguard.universal.Config;
 import me.ishift.epicguard.universal.ServerType;
-import me.ishift.epicguard.universal.cloud.CloudGet;
 import me.ishift.epicguard.universal.cloud.CloudManager;
 import me.ishift.epicguard.universal.util.Logger;
 
 public class CloudTask implements Runnable {
     @Override
     public void run() {
-        if (Config.cloudEnabled) {
-            if (Config.cloudBlacklist) {
-                CloudManager.connect(CloudGet.BLACKLIST);
-                CloudManager.getCloudBlacklist().forEach(BlacklistManager::blacklist);
-            }
+        if (Config.cloudEnabled && Config.cloudBlacklist) {
+            CloudManager.connect();
+            CloudManager.getCloudBlacklist().forEach(BlacklistManager::blacklist);
         }
         Logger.create(ServerType.SPIGOT);
     }
--- src/main/java/me/ishift/epicguard/universal/cloud/CloudManager.java
+++ src/main/java/me/ishift/epicguard/universal/cloud/CloudManager.java
@@ -10,21 +10,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CloudManager {
-    private static final String CLOUD_BL_URL = "https://raw.githubusercontent.com/PolskiStevek/EpicGuard/master/files/cloud/blacklist.txt";
-    private static List<String> CLOUD_BL = new ArrayList<>();
+    private static final String CLOUD_URL = "https://raw.githubusercontent.com/PolskiStevek/EpicGuard/master/files/cloud/blacklist.txt";
+    private static List<String> cloudBlacklist = new ArrayList<>();
     private static boolean online = false;
     private static String lastCheck;
 
-    public static void connect(CloudGet cloudGet) {
-        if (cloudGet == CloudGet.BLACKLIST) {
-            CLOUD_BL = getList(CLOUD_BL_URL);
-        }
-    }
-
-    private static List<String> getList(String url) {
+    public static void connect() {
         try {
             Logger.debug("[CLOUD] Checking data from the cloud...");
-            final Scanner s = new Scanner(new URL(url).openStream());
+            final Scanner s = new Scanner(new URL(CLOUD_URL).openStream());
             final List<String> list = new ArrayList<>();
             if (s.hasNext()) {
                 while (s.hasNextLine()) {
@@ -35,17 +29,16 @@ public class CloudManager {
             online = true;
             lastCheck = new SimpleDateFormat("HH:mm").format(new Date());
             Logger.debug("[CLOUD] Operation succesfully completed.");
-            return list;
+            cloudBlacklist = list;
         } catch (Exception e) {
             Logger.info("[CLOUD] Error occurred in cloud connecton!");
             Logger.info("[CLOUD] " + e.getMessage());
             online = false;
         }
-        return new ArrayList<>();
     }
 
     public static List<String> getCloudBlacklist() {
-        return CLOUD_BL;
+        return cloudBlacklist;
     }
 
     public static boolean isOnline() {
