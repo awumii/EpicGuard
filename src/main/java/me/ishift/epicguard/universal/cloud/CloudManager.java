7fe0ad9b540027afa87ac50e8dcd9eece90451da
PolskiStevek <polskistevek@op.pl>
2020-02-24 21:51:16 +0100

    Improved CloudManager.
    Former-commit-id: 5e1d9a2b66880dc9b504094aefde4c8aee5139d4

--- src/main/java/me/ishift/epicguard/universal/cloud/CloudManager.java
+++ src/main/java/me/ishift/epicguard/universal/cloud/CloudManager.java
@@ -1,13 +1,12 @@
 package me.ishift.epicguard.universal.cloud;
 
 import me.ishift.epicguard.universal.util.Logger;
+import me.ishift.epicguard.universal.util.URLUtil;
 
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
 
 public class CloudManager {
     private static final String CLOUD_URL = "https://raw.githubusercontent.com/PolskiStevek/EpicGuard/master/files/cloud/blacklist.txt";
@@ -17,24 +16,11 @@ public class CloudManager {
 
     public static void connect() {
         try {
-            final Scanner s = new Scanner(new URL(CLOUD_URL).openStream());
-            final List<String> list = new ArrayList<>();
-            if (s.hasNext()) {
-                while (s.hasNextLine()) {
-                    list.add(s.nextLine());
-                }
-            }
-            // Do not update blacklist if there is no change in cloud.
-            if (cloudBlacklist.size() == list.size()) return;
-
-            Logger.debug("[CLOUD] Checking data from the cloud...");
-            s.close();
+            cloudBlacklist = URLUtil.readLines(CLOUD_URL);
             online = true;
             lastCheck = new SimpleDateFormat("HH:mm").format(new Date());
-            Logger.debug("[CLOUD] Operation succesfully completed.");
-            cloudBlacklist = list;
         } catch (Exception e) {
-            Logger.info("[CLOUD] Error occurred in cloud connecton!");
+            Logger.info("[CLOUD] An error occurred, please contact author!");
             Logger.info("[CLOUD] " + e.getMessage());
             online = false;
         }
