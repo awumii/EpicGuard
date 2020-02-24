f052aa708513ee58061ad7fcf1ff2988a23d7fda
PolskiStevek <polskistevek@op.pl>
2020-02-24 16:51:15 +0100

    Improved ProxyCheck.
    Former-commit-id: b4425bce9821a0b5faca190b819d5718c7311190

--- src/main/java/me/ishift/epicguard/universal/check/ProxyCheck.java
+++ src/main/java/me/ishift/epicguard/universal/check/ProxyCheck.java
@@ -1,31 +1,16 @@
 package me.ishift.epicguard.universal.check;
 
 import me.ishift.epicguard.universal.Config;
-import me.ishift.epicguard.universal.util.Logger;
+import me.ishift.epicguard.universal.util.URLUtil;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.Scanner;
 
 public class ProxyCheck {
 
     public static boolean check(String adress) {
         final String url = "http://proxycheck.io/v2/" + adress + "?key=" + Config.apiKey;
-
         try {
-            final Scanner scanner = new Scanner(new URL(url).openStream());
-            Logger.debug("# Checking proxy from URL: " + url);
-            if (scanner.hasNextLine()) {
-                while (scanner.hasNext()) {
-                    if (scanner.next().contains("yes")) {
-                        Logger.debug("# Detected Proxy, URL: " + url);
-                        return true;
-                    }
-                }
-                Logger.debug("# Proxy is not detected from: " + url);
-                scanner.close();
-                return false;
-            }
+            return URLUtil.readLines(url).contains("yes");
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/me/ishift/epicguard/universal/util/URLUtil.java
+++ src/main/java/me/ishift/epicguard/universal/util/URLUtil.java
@@ -1,4 +1,29 @@
 package me.ishift.epicguard.universal.util;
 
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class URLUtil {
+    public static String readString(String requestURL) throws IOException {
+        try (Scanner scanner = new Scanner(new URL(requestURL).openStream(), StandardCharsets.UTF_8.toString())) {
+            scanner.useDelimiter("\\A");
+            return scanner.hasNext() ? scanner.next() : "";
+        }
+    }
+
+    public static List<String> readLines(String requestURL) throws IOException {
+        final Scanner scanner = new Scanner(new URL(requestURL).openStream(), StandardCharsets.UTF_8.toString());
+        final List<String> lines = new ArrayList<>();
+        if (scanner.hasNextLine()) {
+            while (scanner.hasNext()) {
+                lines.add(scanner.next());
+            }
+        }
+        scanner.close();
+        return lines;
+    }
 }
