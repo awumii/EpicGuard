1e63cddbbd4263d87835430dbd4eefc0fa1a1848
PolskiStevek <polskistevek@op.pl>
2020-02-25 19:38:17 +0100

    Removed messages mirror.
    Former-commit-id: f26495daf0525d1269c31486fb44d4c71ce82a9c

--- src/main/java/me/ishift/epicguard/universal/cloud/Downloader.java
+++ src/main/java/me/ishift/epicguard/universal/cloud/Downloader.java
@@ -11,7 +11,6 @@ import java.nio.channels.ReadableByteChannel;
 
 public class Downloader {
     public static final String MIRROR_GEO = "https://github.com/PolskiStevek/EpicGuard/raw/master/files/GeoLite2-Country.mmdb";
-    public static final String MIRROR_MESSAGES = "https://raw.githubusercontent.com/PolskiStevek/EpicGuard/master/files/messages_en_US.yml";
 
     public static void download(String from, String to) throws IOException {
         final File file = new File(to);
