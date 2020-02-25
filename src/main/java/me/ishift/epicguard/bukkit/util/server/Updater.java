50f8f927ca101fe868c78a8af18c5fd7384fa0c3
PolskiStevek <polskistevek@op.pl>
2020-02-25 18:14:43 +0100

    Improved UpdateChecker.

--- src/main/java/me/ishift/epicguard/bukkit/util/server/Updater.java
+++ src/main/java/me/ishift/epicguard/bukkit/util/server/Updater.java
@@ -4,41 +4,36 @@ import me.ishift.epicguard.bukkit.GuardBukkit;
 import me.ishift.epicguard.bukkit.util.misc.MessagesBukkit;
 import me.ishift.epicguard.universal.Config;
 import me.ishift.epicguard.universal.util.ChatUtil;
+import me.ishift.epicguard.universal.util.URLUtil;
 import org.bukkit.entity.Player;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.Scanner;
 
 public class Updater {
-    private static final String currentVersion = GuardBukkit.getInstance().getDescription().getVersion();
-    private static String lastestVersion;
-    private static boolean updateAvaible = false;
+    private static final String CURRENT_VERSION = GuardBukkit.getInstance().getDescription().getVersion();
+    private static final String UPDATE_URL = "https://raw.githubusercontent.com/PolskiStevek/EpicGuard/master/files/version.info";
+    private static String latestVersion;
+    private static boolean updateAvailable = false;
 
     public static String getCurrentVersion() {
-        return currentVersion;
+        return CURRENT_VERSION;
     }
 
     public static void checkForUpdates() {
         if (Config.updater) {
-            lookup();
-            updateAvaible = !lastestVersion.equals(currentVersion);
+            try {
+                latestVersion = URLUtil.readString(UPDATE_URL);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            updateAvailable = !latestVersion.equals(CURRENT_VERSION);
         }
     }
 
     public static void notify(Player p) {
-        if (Config.updater && p.hasPermission(GuardBukkit.PERMISSION) && updateAvaible) {
-            p.sendMessage(ChatUtil.fix(MessagesBukkit.PREFIX + "&7Your EpicGuard version &8(&c" + currentVersion + "&8) &7is outdated! New version is available on SpigotMC &8(&6" + Updater.lastestVersion + "&8)&7."));
-            p.sendMessage(ChatUtil.fix(MessagesBukkit.PREFIX + "&7Download latest version, to enjoy new features: &6https://www.spigotmc.org/resources/72369"));
-        }
-    }
-
-    private static void lookup() {
-        try {
-            final Scanner scanner = new Scanner(new URL("https://raw.githubusercontent.com/PolskiStevek/EpicGuard/master/files/version.info").openStream());
-            lastestVersion = scanner.next();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (Config.updater && p.hasPermission(GuardBukkit.PERMISSION) && updateAvailable) {
+            p.sendMessage(ChatUtil.fix(MessagesBukkit.PREFIX + "&cOutdated version &8(&6" + CURRENT_VERSION + "&8)&c! New version is available &8(&6" + Updater.latestVersion + "&8)"));
+            p.sendMessage(ChatUtil.fix(MessagesBukkit.PREFIX + "&cPlease download update here: &6https://www.spigotmc.org/resources/72369"));
         }
     }
 }
