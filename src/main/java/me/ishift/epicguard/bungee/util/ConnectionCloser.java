c8036bd269a8b83949fd1531b7b61654687c25b5
PolskiStevek <polskistevek@op.pl>
2020-02-26 14:59:31 +0100

    Improved bungee ConnectionCloser.
    Former-commit-id: de5f603280658b2fca0c2961804a255d12aa67c9

--- src/main/java/me/ishift/epicguard/bungee/util/ConnectionCloser.java
+++ src/main/java/me/ishift/epicguard/bungee/util/ConnectionCloser.java
@@ -1,41 +1,19 @@
 package me.ishift.epicguard.bungee.util;
 
 import me.ishift.epicguard.bungee.GuardBungee;
-import me.ishift.epicguard.universal.Messages;
 import me.ishift.epicguard.universal.check.detection.SpeedCheck;
 import me.ishift.epicguard.universal.types.Reason;
-import me.ishift.epicguard.universal.util.ChatUtil;
 import me.ishift.epicguard.universal.Logger;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.PendingConnection;
 
-import java.util.stream.Collectors;
-
 public class ConnectionCloser {
     public static void close(PendingConnection connection, Reason reason) {
+        connection.disconnect(new TextComponent(reason.getReason()));
         if (GuardBungee.log) {
             Logger.info("Closing: " + connection.getVirtualHost().getAddress().getHostAddress() + "(" + connection.getName() + "), (" + reason + ")]");
         }
 
         SpeedCheck.setTotalBots(SpeedCheck.getTotalBots() + 1);
-        if (reason == Reason.GEO) {
-            connection.disconnect(new TextComponent(ChatUtil.fix(Messages.messageKickCountry.stream().map(s -> s + "\n").collect(Collectors.joining()))));
-        }
-
-        if (reason == Reason.ATTACK) {
-            connection.disconnect(new TextComponent(ChatUtil.fix(Messages.messageKickAttack.stream().map(s -> s + "\n").collect(Collectors.joining()))));
-        }
-
-        if (reason == Reason.PROXY) {
-            connection.disconnect(new TextComponent(ChatUtil.fix(Messages.messageKickProxy.stream().map(s -> s + "\n").collect(Collectors.joining()))));
-        }
-
-        if (reason == Reason.BLACKLIST) {
-            connection.disconnect(new TextComponent(ChatUtil.fix(Messages.messageKickBlacklist.stream().map(s -> s + "\n").collect(Collectors.joining()))));
-        }
-
-        if (reason == Reason.NAMECONTAINS) {
-            connection.disconnect(new TextComponent(ChatUtil.fix(Messages.messageKickNamecontains.stream().map(s -> s + "\n").collect(Collectors.joining()))));
-        }
     }
 }
