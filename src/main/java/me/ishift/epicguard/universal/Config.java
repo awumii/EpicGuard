9ee572d4724cba449f17913488bdaef12fa72b46
PolskiStevek <polskistevek@op.pl>
2020-03-01 20:55:21 +0100

    Changed Config class.
    Former-commit-id: 4e66b15c313eda48911131a03c1005091e28b9ba

--- src/main/java/me/ishift/epicguard/universal/Config.java
+++ src/main/java/me/ishift/epicguard/universal/Config.java
@@ -3,7 +3,6 @@ package me.ishift.epicguard.universal;
 import de.leonhard.storage.Yaml;
 import de.leonhard.storage.internal.settings.ConfigSettings;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Config {
@@ -13,40 +12,52 @@ public class Config {
     public static String firewallBlacklistCommand;
     public static String firewallWhitelistCommand;
     public static boolean firewallEnabled;
+
     public static int connectSpeed;
     public static int pingSpeed;
+
     public static boolean autoWhitelist;
     public static int autoWhitelistTime;
+
     public static String apiKey;
+
     public static List<String> countryList;
-    public static List<String> blockedNames;
     public static String countryMode;
+
+    public static List<String> blockedNames;
     public static boolean updater;
     public static long attackResetTimer;
     public static boolean tabCompleteBlock;
     public static boolean bandwidthOptimizer;
+
     public static List<String> blockedCommands;
     public static List<String> allowedCommands;
+
     public static List<String> opProtectionList;
     public static String opProtectionAlert;
     public static String opProtectionCommand;
+
     public static boolean allowedCommandsBypass;
     public static boolean blockedCommandsEnable;
     public static boolean allowedCommandsEnable;
+
     public static boolean opProtectionEnable;
     public static boolean ipHistoryEnable;
-    public static boolean forceRejoin;
+    public static boolean rejoinCheck;
     public static boolean pexProtection;
     public static boolean serverListCheck;
 
     public static boolean filterEnabled;
     public static List<String> filterValues;
+
     public static boolean channelVerification;
     public static long channelDelay;
     public static String channelPunish;
+
     public static boolean blockedBrands;
     public static String blockedBrandsPunish;
     public static List<String> blockedBrandsValues;
+
     public static boolean customTabComplete;
     public static List<String> customTabCompleteList;
     public static boolean customTabCompleteBypass;
@@ -54,58 +65,59 @@ public class Config {
     public static void loadBukkit() {
         BUKKIT.setConfigSettings(ConfigSettings.PRESERVE_COMMENTS);
 
-        Config.firewallEnabled = BUKKIT.getBoolean("firewall");
-        Config.firewallBlacklistCommand = BUKKIT.getString("firewall.command-blacklist");
-        Config.firewallWhitelistCommand = BUKKIT.getString("firewall.command-whitelist");
-        Config.connectSpeed = BUKKIT.getInt("speed.connection");
-        Config.pingSpeed = BUKKIT.getInt("speed.ping-speed");
-        Config.autoWhitelist = BUKKIT.getBoolean("auto-whitelist.enabled");
-        Config.autoWhitelistTime = BUKKIT.getInt("auto-whitelist.time");
-        Config.apiKey = BUKKIT.getString("antibot.api-key");
-        Config.countryList = BUKKIT.getStringList("countries.list");
-        Config.countryMode = BUKKIT.getString("countries.mode");
-        Config.updater = BUKKIT.getBoolean("updater");
-        Config.attackResetTimer = BUKKIT.getLong("speed.attack-timer-reset");
-        Config.tabCompleteBlock = BUKKIT.getBoolean("fully-block-tab-complete");
-        Config.blockedCommands = BUKKIT.getStringList("command-protection.list");
-        Config.allowedCommands = BUKKIT.getStringList("allowed-commands.list");
-        Config.opProtectionList = BUKKIT.getStringList("op-protection.list");
-        Config.opProtectionAlert = BUKKIT.getString("op-protection.alert");
-        Config.opProtectionCommand = BUKKIT.getString("op-protection.command");
-        Config.blockedCommandsEnable = BUKKIT.getBoolean("command-protection.enabled");
-        Config.allowedCommandsEnable = BUKKIT.getBoolean("allowed-commands.enabled");
-        Config.opProtectionEnable = BUKKIT.getBoolean("op-protection.enabled");
-        Config.ipHistoryEnable = BUKKIT.getBoolean("ip-history.enabled");
-        Config.forceRejoin = BUKKIT.getBoolean("antibot.force-rejoin");
-        Config.pexProtection = BUKKIT.getBoolean("op-protection.pex-protection");
-        Config.blockedNames = BUKKIT.getStringList("antibot.name-contains");
-        Config.filterEnabled = BUKKIT.getBoolean("console-filter.enabled");
-        Config.filterValues = BUKKIT.getStringList("console-filter.messages");
-        Config.bandwidthOptimizer = BUKKIT.getBoolean("bandwidth-optimizer");
-        Config.customTabComplete = BUKKIT.getBoolean("custom-tab-complete.enabled");
-        Config.customTabCompleteList = BUKKIT.getStringList("custom-tab-complete.list");
-        Config.allowedCommandsBypass = BUKKIT.getBoolean("bypass.allowed-commands");
-        Config.customTabCompleteBypass = BUKKIT.getBoolean("bypass.custom-tab-complete");
+        firewallEnabled = BUKKIT.getBoolean("firewall");
+        firewallBlacklistCommand = BUKKIT.getString("firewall.command-blacklist");
+        firewallWhitelistCommand = BUKKIT.getString("firewall.command-whitelist");
+        connectSpeed = BUKKIT.getInt("speed.connection");
+        pingSpeed = BUKKIT.getInt("speed.ping-speed");
+        autoWhitelist = BUKKIT.getBoolean("auto-whitelist.enabled");
+        autoWhitelistTime = BUKKIT.getInt("auto-whitelist.time");
+        apiKey = BUKKIT.getString("antibot.api-key");
+        countryList = BUKKIT.getStringList("countries.list");
+        countryMode = BUKKIT.getString("countries.mode");
+        updater = BUKKIT.getBoolean("updater");
+        attackResetTimer = BUKKIT.getLong("speed.attack-timer-reset");
+        tabCompleteBlock = BUKKIT.getBoolean("fully-block-tab-complete");
+        blockedCommands = BUKKIT.getStringList("command-protection.list");
+        allowedCommands = BUKKIT.getStringList("allowed-commands.list");
+        opProtectionList = BUKKIT.getStringList("op-protection.list");
+        opProtectionAlert = BUKKIT.getString("op-protection.alert");
+        opProtectionCommand = BUKKIT.getString("op-protection.command");
+        blockedCommandsEnable = BUKKIT.getBoolean("command-protection.enabled");
+        allowedCommandsEnable = BUKKIT.getBoolean("allowed-commands.enabled");
+        opProtectionEnable = BUKKIT.getBoolean("op-protection.enabled");
+        ipHistoryEnable = BUKKIT.getBoolean("ip-history.enabled");
+        pexProtection = BUKKIT.getBoolean("op-protection.pex-protection");
+        blockedNames = BUKKIT.getStringList("antibot.name-contains");
+        filterEnabled = BUKKIT.getBoolean("console-filter.enabled");
+        filterValues = BUKKIT.getStringList("console-filter.messages");
+        bandwidthOptimizer = BUKKIT.getBoolean("bandwidth-optimizer");
+        customTabComplete = BUKKIT.getBoolean("custom-tab-complete.enabled");
+        customTabCompleteList = BUKKIT.getStringList("custom-tab-complete.list");
+        allowedCommandsBypass = BUKKIT.getBoolean("bypass.allowed-commands");
+        customTabCompleteBypass = BUKKIT.getBoolean("bypass.custom-tab-complete");
 
         serverListCheck = BUKKIT.getOrSetDefault("antibot.server-list-check", true);
+        rejoinCheck = BUKKIT.getOrSetDefault("antibot.rejoin-check", true);
     }
 
     public static void loadBungee() {
         BUNGEE.setConfigSettings(ConfigSettings.PRESERVE_COMMENTS);
-        Config.firewallEnabled = BUNGEE.getBoolean("firewall");
-        Config.firewallBlacklistCommand = BUNGEE.getString("firewall.command-blacklist");
-        Config.firewallWhitelistCommand = BUNGEE.getString("firewall.command-whitelist");
-        Config.connectSpeed = BUNGEE.getInt("speed.connection");
-        Config.pingSpeed = BUNGEE.getInt("speed.ping-speed");
-        Config.apiKey = BUNGEE.getString("antibot.api-key");
-        Config.countryList = BUNGEE.getStringList("countries.list");
-        Config.countryMode = BUNGEE.getString("countries.mode");
-        Config.attackResetTimer = BUNGEE.getLong("speed.attack-timer-reset");
-        Config.blockedNames = BUNGEE.getStringList("antibot.name-contains");
-        Config.filterEnabled = BUNGEE.getBoolean("console-filter.enabled");
-        Config.filterValues = BUNGEE.getStringList("console-filter.messages");
-        Config.bandwidthOptimizer = BUNGEE.getBoolean("bandwidth-optimizer");
+        firewallEnabled = BUNGEE.getBoolean("firewall");
+        firewallBlacklistCommand = BUNGEE.getString("firewall.command-blacklist");
+        firewallWhitelistCommand = BUNGEE.getString("firewall.command-whitelist");
+        connectSpeed = BUNGEE.getInt("speed.connection");
+        pingSpeed = BUNGEE.getInt("speed.ping-speed");
+        apiKey = BUNGEE.getString("antibot.api-key");
+        countryList = BUNGEE.getStringList("countries.list");
+        countryMode = BUNGEE.getString("countries.mode");
+        attackResetTimer = BUNGEE.getLong("speed.attack-timer-reset");
+        blockedNames = BUNGEE.getStringList("antibot.name-contains");
+        filterEnabled = BUNGEE.getBoolean("console-filter.enabled");
+        filterValues = BUNGEE.getStringList("console-filter.messages");
+        bandwidthOptimizer = BUNGEE.getBoolean("bandwidth-optimizer");
 
         serverListCheck = BUKKIT.getOrSetDefault("antibot.server-list-check", true);
+        rejoinCheck = BUKKIT.getOrSetDefault("antibot.rejoin-check", true);
     }
 }
