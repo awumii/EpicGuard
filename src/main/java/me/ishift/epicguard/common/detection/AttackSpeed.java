46af415e297fe775c1f62c1dcb7675be9563be14
PolskiStevek <polskistevek@op.pl>
2020-03-19 22:33:36 +0100

    Some improvements.
    Former-commit-id: eb6c919dd8103ea1a9554020680a33bcdee68ba7

--- src/main/java/me/ishift/epicguard/common/detection/AttackSpeed.java
+++ src/main/java/me/ishift/epicguard/common/detection/AttackSpeed.java
@@ -13,21 +13,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-/*
- * EpicGuard is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * EpicGuard is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
-
 package me.ishift.epicguard.common.detection;
 
 import me.ishift.epicguard.common.types.CounterType;
@@ -52,15 +37,6 @@ public class AttackSpeed {
         }
         if (type == CounterType.PING) {
             AttackSpeed.setPingPerSecond(AttackSpeed.getPingPerSecond() + 1);
-        }
-    }
-
-    public static void decrease(CounterType type) {
-        if (type == CounterType.CONNECT && AttackSpeed.getConnectPerSecond() != 0) {
-            AttackSpeed.setConnectPerSecond(AttackSpeed.getConnectPerSecond() - 1);
-        }
-        if (type == CounterType.PING && AttackSpeed.getPingPerSecond() != 0) {
-            AttackSpeed.setPingPerSecond(AttackSpeed.getPingPerSecond() - 1);
         }
     }
 
