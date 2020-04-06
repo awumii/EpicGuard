b9cd0c0bf36c7cd1c8ad764127bb551af12c8db6
PolskiStevek <polskistevek@op.pl>
2020-04-06 21:40:54 +0200

    Documented the LibraryLoader class.

--- src/main/java/me/ishift/epicguard/common/util/LibraryLoader.java
+++ src/main/java/me/ishift/epicguard/common/util/LibraryLoader.java
@@ -22,15 +22,20 @@ import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
-/**
- * Velocity does not include mysql/jdbc, so we need to download it and load.
- */
-public class DependencyLoader {
-    public static void load() {
+public class LibraryLoader {
+    /**
+     * Loads the required libraries for the EpicGuard.
+     */
+    public static void init() {
         tryDownload("com.zaxxer.hikari.HikariDataSource", "https://repo1.maven.org/maven2/com/zaxxer/HikariCP/3.4.2/HikariCP-3.4.2.jar", "HikariCP-3.4.2");
         tryDownload("com.mysql.jdbc.Driver", "https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.19/mysql-connector-java-8.0.19.jar", "mysql-connector-java-8.0.19");
     }
 
+    /**
+     * @param className If the speciefied class is not found, library will be downloaded.
+     * @param url URL from where library will be downloaded.
+     * @param fileName Name of the final jar WITHOUT the extension.
+     */
     public static void tryDownload(String className, String url, String fileName) {
         try {
             Class.forName(className);
@@ -50,6 +55,11 @@ public class DependencyLoader {
         }
     }
 
+    /**
+     * Loads specified jar file to the runtime.
+     *
+     * @param file The jar file to be loaded.
+     */
     public static void loadLibrary(File file) {
         try {
             final URL url = file.toURI().toURL();
