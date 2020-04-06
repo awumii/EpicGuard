550cf952a3f1a67805212de957f89dd39df17a28
PolskiStevek <polskistevek@op.pl>
2020-04-06 21:49:02 +0200

    Added more methods to the FileUtil.

--- src/main/java/me/ishift/epicguard/common/util/FileUtil.java
+++ src/main/java/me/ishift/epicguard/common/util/FileUtil.java
@@ -1,11 +1,49 @@
 package me.ishift.epicguard.common.util;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 
 public class FileUtil {
+    /**
+     * Erases content of a text file, without deleting it.
+     *
+     * @param file File which should be erased.
+     */
+    public void eraseFile(File file) {
+        try {
+            final PrintWriter pw = new PrintWriter(file);
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Appending a message to the text file.
+     *
+     * @param file Target file.
+     * @param message Message to be logged.
+     */
+    public void writeToFile(File file, String message) {
+        try {
+            if (!file.exists() || file.createNewFile()) {
+                return;
+            }
+            final BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true));
+            bufferedWriter.append(message);
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * This method will copy resource from jar file to specified directory.
      * @param directory Directory where file will be copied. If not exists, will be automatically created.
