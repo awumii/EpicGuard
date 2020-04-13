f42bd92fde64cbeb1450b80bd1c0d53403a68b58
PolskiStevek <polskistevek@op.pl>
2020-04-13 17:50:58 +0200

    Removed config re-generator to prevent from weird issues.
    Former-commit-id: 3af8339658873cdc93fc1ef34762ea2a2c722cea

--- src/main/java/me/ishift/epicguard/bungee/EpicGuardBungee.java
+++ src/main/java/me/ishift/epicguard/bungee/EpicGuardBungee.java
@@ -1,3 +1,18 @@
+/*
+ * EpicGuard is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EpicGuard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package me.ishift.epicguard.bungee;
 
 import me.ishift.epicguard.bungee.command.GuardCommand;
@@ -5,7 +20,6 @@ import me.ishift.epicguard.bungee.listener.PingListener;
 import me.ishift.epicguard.bungee.listener.PostLoginListener;
 import me.ishift.epicguard.bungee.listener.PreLoginListener;
 import me.ishift.epicguard.bungee.util.BungeeMetrics;
-import me.ishift.epicguard.common.data.StorageManager;
 import me.ishift.epicguard.common.data.config.Configuration;
 import me.ishift.epicguard.common.detection.AttackManager;
 import me.ishift.epicguard.common.task.AttackToggleTask;
@@ -14,13 +28,6 @@ import me.ishift.epicguard.common.util.FileUtil;
 import me.ishift.epicguard.common.util.Log4jFilter;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginManager;
-import net.md_5.bungee.api.scheduler.TaskScheduler;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -34,7 +41,6 @@ public class EpicGuardBungee extends Plugin {
     public void onEnable() {
         epicGuardBungee = this;
         this.statusPlayers = new ArrayList<>();
-        Configuration.checkVersion();
         FileUtil.saveResource(this.getDataFolder(), "config.yml");
         AttackManager.init();
 
