8f1ff116ff5956be6d278c534956d4bfa6370e49
xishift <polskistevek@op.pl>
2020-07-02 21:53:24 +0200

    Added InventoryAPI to the source.
    Former-commit-id: 7051d5c1123ba3295f9a249c3138100f3b4b98f6

---
+++ src/main/java/me/ishift/inventory/api/inventories/ClickableCloseableInventory.java
@@ -0,0 +1,49 @@
+/*
+ * InventoryAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * InventoryAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package me.ishift.inventory.api.inventories;
+
+import me.ishift.inventory.api.InventorySize;
+import me.ishift.inventory.api.basic.Clickable;
+import me.ishift.inventory.api.basic.Closeable;
+import me.ishift.inventory.api.basic.BaseInventory;
+import me.ishift.inventory.api.util.MessageUtil;
+
+public abstract class ClickableCloseableInventory implements BaseInventory, Clickable, Closeable {
+    private final String title;
+    private final int size;
+    private final String id;
+
+    public ClickableCloseableInventory(String title, String id, InventorySize size) {
+        this.title = MessageUtil.fixColor(title);
+        this.size = size.getSize();
+        this.id = id;
+    }
+
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
+
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+}
