1ed408fa65e55f4fc3abca8464525e1c69c6d03c
xishift <polskistevek@op.pl>
2020-07-04 22:29:41 +0200

    Bungee implementation (not finished yet)
    Former-commit-id: 57260009b4f387dd195d373d1c82ae5ded7cfbd4

---
+++ src/main/java/me/ishift/epicguard/bungee/BungeeNotificator.java
@@ -0,0 +1,16 @@
+package me.ishift.epicguard.bungee;
+
+import me.ishift.epicguard.core.util.ChatUtils;
+import me.ishift.epicguard.core.util.Notificator;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import java.util.UUID;
+
+public class BungeeNotificator implements Notificator {
+    @Override
+    public void sendActionBar(String message, UUID target) {
+        ProxyServer.getInstance().getPlayer(target).sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ChatUtils.colored(message)));
+    }
+}
---
+++ src/main/java/me/ishift/epicguard/bungee/EpicGuardBungee.java
@@ -0,0 +1,26 @@
+package me.ishift.epicguard.bungee;
+
+import me.ishift.epicguard.core.EpicGuard;
+import me.ishift.epicguard.core.task.CounterTask;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.scheduler.TaskScheduler;
+
+import java.util.concurrent.TimeUnit;
+
+public class EpicGuardBungee extends Plugin {
+    private EpicGuard epicGuard;
+
+    @Override
+    public void onEnable() {
+        this.epicGuard = new EpicGuard(this.getLogger(), new BungeeNotificator());
+
+        TaskScheduler scheduler = this.getProxy().getScheduler();
+        scheduler.schedule(this, new CounterTask(this.epicGuard), 1L, TimeUnit.SECONDS);
+        scheduler.schedule(this, new CounterTask(this.epicGuard), 20L, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void onDisable() {
+        this.epicGuard.shutdown();
+    }
+}
