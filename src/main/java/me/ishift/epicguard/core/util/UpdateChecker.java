01a894807b85fdb0010d8b9c64bfd1b2ec4b7b4c
xishift <polskistevek@op.pl>
2020-07-13 16:13:32 +0200

    Added update checker.
    Former-commit-id: d6d77aaa42a8e2d345fd5d1d105a28f879b93208

---
+++ src/main/java/me/ishift/epicguard/core/util/UpdateChecker.java
@@ -0,0 +1,27 @@
+package me.ishift.epicguard.core.util;
+
+import me.ishift.epicguard.core.EpicGuard;
+
+public final class UpdateChecker {
+    private static final String CHECK_URL = "https://raw.githubusercontent.com/xishift/EpicGuard/master/files/version.info";
+    private static String remoteVersion;
+    private static boolean available;
+
+    public static void checkForUpdates(EpicGuard epicGuard) {
+        remoteVersion = URLUtils.readString(CHECK_URL);
+        int latest = Integer.parseInt(remoteVersion.replace(".", ""));
+        int current = Integer.parseInt(epicGuard.getVersion().replace(".", ""));
+
+        available = latest > current;
+    }
+
+    public static boolean isAvailable() {
+        return available;
+    }
+
+    public static String getRemoteVersion() {
+        return remoteVersion;
+    }
+
+    private UpdateChecker() {}
+}
