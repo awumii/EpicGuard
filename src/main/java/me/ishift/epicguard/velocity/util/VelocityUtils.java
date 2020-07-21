0bc21e255ce7fd25485f33b150b02e7d0b4b05f1
xishift <polskistevek@op.pl>
2020-07-21 18:17:41 +0200

    Added support for Velocity proxy.

---
+++ src/main/java/me/ishift/epicguard/velocity/util/VelocityUtils.java
@@ -0,0 +1,19 @@
+package me.ishift.epicguard.velocity.util;
+
+import com.velocitypowered.api.command.CommandSource;
+import net.kyori.text.TextComponent;
+
+public class VelocityUtils {
+    public static String color(String message) {
+        // Is there something similar to Bukkit & Bungee - ChatColor.translateAlternateColorCodes()?
+        return message.replace("&", "§");
+    }
+
+    public static TextComponent getTextComponent(String string) {
+        return TextComponent.of(color(string));
+    }
+
+    public static void sendMessage(CommandSource source, String message) {
+        source.sendMessage(getTextComponent(message));
+    }
+}
