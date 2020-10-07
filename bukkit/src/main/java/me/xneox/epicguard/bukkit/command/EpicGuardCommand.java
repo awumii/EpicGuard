c710e27c4c74372cb92d831bca332f36cbfc6337
xxneox <polskistevek@op.pl>
2020-10-07 16:27:23 +0200

    Added totalBots stat to the stats command.
    Former-commit-id: 44aab61b0d3c4b43cf9695bd0db9c1f83c07fd08

--- bukkit/src/main/java/me/xneox/epicguard/bukkit/command/EpicGuardCommand.java
+++ bukkit/src/main/java/me/xneox/epicguard/bukkit/command/EpicGuardCommand.java
@@ -82,7 +82,8 @@ public class EpicGuardCommand implements CommandExecutor, TabCompleter {
                 send(sender, "");
                 send(sender, " &8• &7Blacklisted IPs: &c&l" + this.epicGuard.getStorageManager().getBlacklist().size());
                 send(sender, " &8• &7Whitelisted IPs: &a&l" + this.epicGuard.getStorageManager().getWhitelist().size());
-                send(sender, " &8• &7Connections: &6&l" + this.epicGuard.getConnectionPerSecond() + "/s");
+                send(sender, " &8• &7Connections: &6&l" + this.epicGuard.getAttackManager().getCPS() + "/s");
+                send(sender, " &8• &7Total connections: &6&l" + this.epicGuard.getAttackManager().getTotalBots() + "/s");
                 send(sender, "");
                 send(sender, "&8&m---------------------------------------------------");
                 break;
