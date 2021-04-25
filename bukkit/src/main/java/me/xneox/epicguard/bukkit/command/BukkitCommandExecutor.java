836367a97ae0da142942e877529891626370e125
xxneox <polskistevek@op.pl>
2021-04-25 12:41:01 +0200

    Further improvements, shading depencencies
    Former-commit-id: d9d8a08af4488727b8521b4c3bfc16ae2e8953ea

--- bukkit/src/main/java/me/xneox/epicguard/bukkit/command/BukkitCommandExecutor.java
+++ bukkit/src/main/java/me/xneox/epicguard/bukkit/command/BukkitCommandExecutor.java
@@ -2,9 +2,10 @@ package me.xneox.epicguard.bukkit.command;
 
 import me.xneox.epicguard.core.command.CommandExecutor;
 import org.bukkit.command.*;
-import org.jetbrains.annotations.NotNull;
+
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 
 public class BukkitCommandExecutor implements org.bukkit.command.CommandExecutor, TabCompleter {
@@ -15,13 +16,13 @@ public class BukkitCommandExecutor implements org.bukkit.command.CommandExecutor
     }
 
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+    public boolean onCommand(@Nonnull CommandSender sender, @Nonnull Command command, @Nonnull String label, @Nonnull String[] args) {
         this.command.onCommand(args, new BukkitSender(sender));
         return true;
     }
 
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+    public @Nullable List<String> onTabComplete(@Nonnull CommandSender sender, @Nonnull Command command, @Nonnull String alias, @Nonnull String[] args) {
         return (List<String>) this.command.onTabComplete(args);
     }
 }
