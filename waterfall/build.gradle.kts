7c9a8f783e50d10b3b719a8006de05cd376b3097
xxneox <29680081+xxneox@users.noreply.github.com>
2021-09-10 16:07:18 +0200

    Waterfall: Download sql drivers on runtime

--- waterfall/build.gradle.kts
+++ waterfall/build.gradle.kts
@@ -3,8 +3,7 @@ dependencies {
 
     implementation("org.bstats:bstats-bungeecord:2.2.1")
     implementation("net.kyori:adventure-platform-bungeecord:4.0.0-SNAPSHOT")
-    implementation("org.xerial:sqlite-jdbc:3.36.0.3") // MySQL driver is present on bungee, but SQLite is not.
-    
+
     compileOnly("io.github.waterfallmc:waterfall-api:1.17-R0.1-SNAPSHOT")
 }
 
@@ -12,9 +11,9 @@ tasks {
     shadowJar {
         archiveFileName.set("EpicGuardWaterfall-${project.version}.jar")
 
-        relocate("net.kyori.adventure", "me.xneox.epicguard.bungee.adventure")
-        relocate("net.kyori.examination", "me.xneox.epicguard.bungee.examination")
-        relocate("org.bstats", "me.xneox.epicguard.bungee.bstats")
+        relocate("net.kyori.adventure", "me.xneox.epicguard.waterfall.adventure")
+        relocate("net.kyori.examination", "me.xneox.epicguard.waterfall.examination")
+        relocate("org.bstats", "me.xneox.epicguard.waterfall.bstats")
     }
 
     processResources {
